d3b6905596ec7cc93c273803905b63e02e22c56d
sAMIIGOSs <mlg32292@mail.ru>
2024-03-16 23:46:24 +0500

    add title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,9 @@
         <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
         <div class="nav-links">
                 {#each nav as link}
-                    <a href={link.href} class="link">{link.title}</a>
+                    {#if link.isShow}
+                        <a href={link.href} class="link">{link.title}</a>
+                    {/if}
                 {/each}
         </div>
     </div>
