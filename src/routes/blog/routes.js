3f423bbec25f059a3733e3280aee4446d6ff79f2
sAMIIGOSs <mlg32292@mail.ru>
2024-03-17 23:47:47 +0500

    add title

--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -5,4 +5,13 @@ export const routes = [
         body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
 
     },
+    {
+
+        "id": 2,
+        
+        "title": "My Second Blog Post",
+        
+        "body": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate"
+        
+    },
 ];
